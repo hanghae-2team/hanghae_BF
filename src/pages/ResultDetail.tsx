34f7c3f0a91817b389c17cd953bf56283915b217
bebusl <bebus1998@naver.com>
2025-09-05 22:55:02 +0900

    ✨ feat: 결과 파일 추가

--- src/pages/ResultDetail.tsx
+++ src/pages/ResultDetail.tsx
@@ -4,91 +4,16 @@ import { useParams } from 'react-router';
 import { getUser } from '@/apis/users';
 import kissingCatSrc from '@/assets/images/result/kissing_cat.svg';
 import wearyCatSrc from '@/assets/images/result/weary_cat.svg';
-import type { MatchingUser } from '@/components/result/MatchingSection';
+import finalResult from '@/assets/result/final-result.json';
 import MatchingSection from '@/components/result/MatchingSection';
 import RollingPaperSection from '@/components/result/RollingPaperSection';
 import UserProfile from '@/components/result/UserProfile';
+import users from '@/data/users.json';
 import { Card } from '@/elements/card';
 import { Layout } from '@/elements/layout';
 import { Separator } from '@/elements/separator';
 import { NotFound } from '@/pages/NotFound';
-
-// Mock Data (이후 API로 대체)
-const getBest5User = (): MatchingUser[] => [
-  {
-    id: 'best1',
-    name: '찰떡 항해인1',
-    image: 'https://picsum.photos/200/300',
-    link: 'https://github.com/best1',
-    hobbies: [],
-  },
-  {
-    id: 'best2',
-    name: '찰떡 항해인2',
-    image: 'https://picsum.photos/200/301',
-    link: 'https://github.com/best2',
-    hobbies: [],
-  },
-  {
-    id: 'best3',
-    name: '찰떡 항해인3',
-    image: 'https://picsum.photos/200/302',
-    link: 'https://github.com/best3',
-    hobbies: [],
-  },
-  {
-    id: 'best4',
-    name: '찰떡 항해인4',
-    image: 'https://picsum.photos/200/303',
-    link: 'https://github.com/best4',
-    hobbies: [],
-  },
-  {
-    id: 'best5',
-    name: '찰떡 항해인5',
-    image: 'https://picsum.photos/200/304',
-    link: 'https://github.com/best5',
-    hobbies: [],
-  },
-];
-
-const getWorst5User = (): MatchingUser[] => [
-  {
-    id: 'worst1',
-    name: '시루떡 항해인1',
-    image: 'https://picsum.photos/200/305',
-    link: 'https://github.com/worst1',
-    hobbies: [],
-  },
-  {
-    id: 'worst2',
-    name: '시루떡 항해인2',
-    image: 'https://picsum.photos/200/306',
-    link: 'https://github.com/worst2',
-    hobbies: [],
-  },
-  {
-    id: 'worst3',
-    name: '시루떡 항해인3',
-    image: 'https://picsum.photos/200/307',
-    link: 'https://github.com/worst3',
-    hobbies: [],
-  },
-  {
-    id: 'worst4',
-    name: '시루떡 항해인4',
-    image: 'https://picsum.photos/200/308',
-    link: 'https://github.com/worst4',
-    hobbies: [],
-  },
-  {
-    id: 'worst5',
-    name: '시루떡 항해인5',
-    image: 'https://picsum.photos/200/309',
-    link: 'https://github.com/worst5',
-    hobbies: [],
-  },
-];
+import type { FinalResultJson, UsersType } from '@/types/result';
 
 export const ResultDetail = () => {
   const { id } = useParams();
@@ -100,8 +25,31 @@ export const ResultDetail = () => {
     enabled: !!id,
   });
 
-  const best5 = getBest5User();
-  const worst5 = getWorst5User();
+  const surveyResult = (finalResult as FinalResultJson)[id as string];
+
+  if (!surveyResult) {
+    return <NotFound />;
+  }
+
+  const best5 = surveyResult.top5.map((result) => {
+    const userInfo = (users as UsersType)[result.targetId];
+    return {
+      ...result,
+      ...userInfo,
+      id: result.targetId,
+      hobbies: [],
+    };
+  });
+
+  const worst5 = surveyResult.bottom5.map((result) => {
+    const userInfo = (users as UsersType)[result.targetId];
+    return {
+      ...result,
+      ...userInfo,
+      id: result.targetId,
+      hobbies: [],
+    };
+  });
 
   if (isError) {
     return <NotFound />;
@@ -128,7 +76,7 @@ export const ResultDetail = () => {
                   시루떡 궁합
                 </h2>
                 <Separator />
-                <MatchingSection title="찰떡 궁합" users={worst5} />
+                <MatchingSection title="시루떡 궁합" users={worst5} />
               </div>
             </Card>
             <RollingPaperSection />
